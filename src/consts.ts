8f9f868cbc3aaf55cd4e9b3721e7560a16c56c92
LadyKerr <ladykerr@github.com>
2025-02-05 21:19:30 -0600

    Add new SVG icons and update site branding and content

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Sorolla';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'That LadyDev | Tech Education';
+export const SITE_DESCRIPTION = 'tech education platform that provides access for ambitious Black women and underrepresented, early career engineers to thrive in tech';
