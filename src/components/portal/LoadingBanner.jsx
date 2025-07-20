f81cdae2b3d8488bbe033eea30a12dac1b5ba812
Robert Ryder <itsrobertdryder@hotmail.com>
2025-07-20 13:28:38 -0400

    wording, part 1 of n

--- src/components/portal/LoadingBanner.jsx
+++ src/components/portal/LoadingBanner.jsx
@@ -3,7 +3,7 @@ function LoadingBanner() {
         <>
           <div class="flex flex-col justify-center justify-self-center items-center">
             <div class="flex flex-col bg-pgreen rounded-xl border-black border-2">
-              <p class="text-4xl p-5 m-5 mb-16 text-center">Application is Loading</p>
+              <p class="text-4xl p-5 m-5 mb-16 text-center">Loading; please wait...</p>
             </div>
           </div>
         </>
